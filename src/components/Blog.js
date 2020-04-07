edc7cd8c5423176c07eaffa1c060746a3e38dcdb
ElMayo <mic.l.meier@gmail.com>
2020-04-07 20:02:44 +0200

    exercise bloglist - step6

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,8 +1,40 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import React, { useState } from "react";
+const Blog = ({ blog }) => {
+  const [showDetails, setShowDetails] = useState(false);
 
-export default Blog
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: "solid",
+    borderWidth: 1,
+    marginBottom: 5,
+  };
+
+  const handleClick = () => {
+    setShowDetails(!showDetails);
+  };
+
+  const standardView = (
+    <div style={blogStyle}>
+      {blog.title} {blog.author} <button onClick={handleClick}>view</button>
+    </div>
+  );
+
+  const detailedView = (
+    <div style={blogStyle}>
+      <div>
+        {blog.title} by {blog.author}{" "}
+        <button onClick={handleClick}>hide</button>
+      </div>
+      <div>{blog.url}</div>
+      <div>
+        {blog.likes} <button>like</button>
+      </div>
+      <div>{blog.user.name}</div>
+    </div>
+  );
+
+  return <div>{showDetails ? detailedView : standardView}</div>;
+};
+
+export default Blog;
